0a1dd660596ee2078396696d12cc92f4d92a2fc8
MASTER OF C <136145964+master28819@users.noreply.github.com>
2025-04-25 15:37:30 +0000

    Modifying Navbar

--- tech/src/NavBar.jsx
+++ tech/src/NavBar.jsx
@@ -5,9 +5,10 @@ import { useState } from "react";
 const NavBar = () => {
 
   const [loginUser, setLoginUser] = useState(true);
+  const [mode, setMode] = useState("dark");
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
+    <nav className={"navbar navbar-expand-lg " + ( mode === "dark" ? "bg-grey" : "bg-light" ) }>
       <div className="container-fluid">
         <a className="navbar-brand logo-text" href="/">Tech2<span>xplore</span></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,30 +17,30 @@ const NavBar = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mx-auto">
             <li className="nav-item">
-              <a className="nav-link p-0 fw-bolder" href="/about"><span className="fs-6">About Us</span></a>
+              <a className="nav-link p-0 fw-bolder" href="/about"><span className={ "fs-6 " + ( mode === "dark" ? "text-light" : "text-dark" ) }>About Us</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link p-0 fw-bolder" href="/bloggers"><span className="fs-6">Bloggers</span></a>
-            </li>x
-            <li className="nav-item">
-              <a className="nav-link p-0 fw-bolder" href="/posts"><span className="fs-6">Posts</span></a>
+              <a className="nav-link p-0 fw-bolder" href="/bloggers"><span className={ "fs-6 " + ( mode === "dark" ? "text-light" : "text-dark" ) }>Bloggers</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link p-0 fw-bolder" href="/trendings"><span className="fs-6">Trendings</span></a>
+              <a className="nav-link p-0 fw-bolder" href="/posts"><span className={ "fs-6 " + ( mode === "dark" ? "text-light" : "text-dark" ) }>Posts</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link p-0 fw-bolder" href="/mass-filter"><span className="fs-6">Filter</span></a>
+              <a className="nav-link p-0 fw-bolder" href="/trendings"><span className={ "fs-6 " + ( mode === "dark" ? "text-light" : "text-dark" ) }>Trendings</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link p-0 fw-bolder" href="/entity"><span className="fs-6">Entity</span></a>
+              <a className="nav-link p-0 fw-bolder" href="/mass-filter"><span className={ "fs-6 " + ( mode === "dark" ? "text-light" : "text-dark" ) }>Filter</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link p-0 fw-bolder" href="/entity"><span className={ "fs-6 " + ( mode === "dark" ? "text-light" : "text-dark" ) }>Entity</span></a>
             </li>
           </ul>
           <ul className="navbar-nav ms-auto">
             {
               loginUser === false ?
                 <>
-                  <li className="nav-item">
-                    <a className="nav-link p-0 fw-bolder" href="/register"><i className="bi bi-person-plus"></i></a>
+                  <li className="nav-item ">
+                    <a className="nav-link p-0 fw-bolder " href="/register"><i className="bi bi-person-plus"></i></a>
                   </li>
                   <li className="nav-item">
                     <a className="nav-link p-0 fw-bolder" href="/login"><i className="bi bi-box-arrow-in-right"></i></a>
@@ -60,6 +61,10 @@ const NavBar = () => {
                   </li>
                 </>
             }
+            
+            <li className="nav-item">
+              <button className="nav-link p-0 fw-bolder" onClick={ () => { setMode( mode === "dark" ? "light" : "dark" ) } }><i className={"bi " + ( mode === "dark" ? "bi-moon-stars-fill" :  "bi-sun-fill"  ) }></i></button>
+            </li>
           </ul>
         </div>
       </div>
